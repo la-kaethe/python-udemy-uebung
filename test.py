59548ef64a04098846dd5641e39fb0863b9730b3
Katharina Schweiger <kathi.schweiger@posteo.de>
2024-01-28 17:55:52 +0100

    variants to print

---
+++ test.py
@@ -0,0 +1,14 @@
+# "" sagen, dass hier kein Code kommt, sondern nur Text : String
+
+#Variant 1
+print("Day 1 - Python Print function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Variant 2
+print("""Day 1 - Python Print function
+The function is declared like this:
+print('what to print')""")
+
+#Variant 3
+print("Day 1 - Python Print function\nThe function is declared like this:\n('what to print')")
